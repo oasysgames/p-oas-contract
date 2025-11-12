64827166fb83999acd959966a2dc3f22c566b365
tak <re795h@gmail.com>
2025-11-12 14:36:13 +0900

    Support free mint for minter contract

--- src/samples/MinterSample.sol
+++ src/samples/MinterSample.sol
@@ -10,6 +10,7 @@ import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Own
  * - Whitelist functionality
  * - Mint cap limitations
  * - Adjustable mint rates
+ * - Support free minting
  * - Upgradeable ownership pattern
  */
 contract MinterSample is OwnableUpgradeable {
@@ -23,6 +24,8 @@ contract MinterSample is OwnableUpgradeable {
     uint256 public mintedAmount;
 
     /// @dev Mint rate expressed as a percentage where 100 means 100%
+    /// @dev between 0 and 10000
+    /// @dev 0 means free mint
     uint16 public mintRate;
 
     /// @dev Default mint rate expressed as a percentage where 100 means 100%
@@ -67,22 +70,20 @@ contract MinterSample is OwnableUpgradeable {
      * Update minted amount after the function execution
      */
     modifier withinMintCap() {
-        uint256 mintAmount = _mintAmount(msg.value);
-        require(
-            mintedAmount + mintAmount <= mintCap,
-            "Total mint cap exceeded"
-        );
-        if (!disableWhitelistCheck) {
-            require(
-                whitelistWithAllowanceMap[msg.sender] >= mintAmount,
-                "Mint cap exceeded"
-            );
-        }
+        uint256 mintAmount = calculateMintAmount(msg.value);
+        _capCheck(mintAmount);
         _;
-        mintedAmount += mintAmount;
-        if (!disableWhitelistCheck) {
-            whitelistWithAllowanceMap[msg.sender] -= mintAmount;
-        }
+        _updateCap(mintAmount);
+    }
+
+    modifier nonFreeMint() {
+        require(!isFreeMint(), "restricted to non-free mint");
+        _;
+    }
+
+    modifier onlyFreeMint() {
+        require(isFreeMint(), "restricted to free mint");
+        _;
     }
 
     /**
@@ -105,8 +106,6 @@ contract MinterSample is OwnableUpgradeable {
         //  uint16 public mintRate; <- this will return 0 in proxy
         mintRate = DEFAULT_MINTRATE;
         disableWhitelistCheck = disableWhitelistCheck_;
-        // Add the owner to the whitelist by default with full allowance
-        whitelistWithAllowanceMap[owner] = mintCap_;
     }
 
     /**
@@ -114,10 +113,19 @@ contract MinterSample is OwnableUpgradeable {
      * @param depositAmount The amount of OAS to deposit (in wei)
      * @notice The caller must send exactly the OAS amount that matches the token amount
      * @notice Contract must have OPERATOR_ROLE to execute this function
+     * @notice Restricted to non-free mint
      */
     function mint(
         uint256 depositAmount
-    ) public payable virtual hasOperatorRole onlyWhitelisted withinMintCap {
+    )
+        public
+        payable
+        virtual
+        hasOperatorRole
+        onlyWhitelisted
+        nonFreeMint
+        withinMintCap
+    {
         _mint(msg.sender, depositAmount);
     }
 
@@ -127,12 +135,49 @@ contract MinterSample is OwnableUpgradeable {
      * @param depositAmount The amount of OAS to deposit (in wei)
      * @notice The caller must send exactly the OAS amount that matches the token amount
      * @notice Contract must have OPERATOR_ROLE to execute this function
+     * @notice Restricted to non-free mint
      */
     function mint(
         address account,
         uint256 depositAmount
-    ) public payable virtual hasOperatorRole onlyWhitelisted withinMintCap {
+    )
+        public
+        payable
+        virtual
+        hasOperatorRole
+        onlyWhitelisted
+        nonFreeMint
+        withinMintCap
+    {
         _mint(account, depositAmount);
+    }
+
+    /**
+     * @dev Free mints POAS tokens for the caller
+     * @param mintAmount The amount of tokens to mint
+     * @notice The caller must send exactly the OAS amount that matches the token amount
+     * @notice Contract must have OPERATOR_ROLE to execute this function
+     * @notice Restricted to free mint
+     */
+    function freeMint(
+        uint256 mintAmount
+    ) public virtual hasOperatorRole onlyWhitelisted onlyFreeMint {
+        _freeMint(msg.sender, mintAmount);
+    }
+
+    /**
+     * @dev Free mints POAS tokens for a specified account
+     * @param account The address that will receive the minted tokens
+     * @param mintAmount The amount of tokens to mint
+     * @notice The caller must send exactly the OAS amount that matches the token amount
+     * @notice Contract must have OPERATOR_ROLE to execute this function
+     * @notice Restricted to free mint
+     */
+    function freeMint(
+        address account,
+        uint256 mintAmount
+    ) public virtual hasOperatorRole onlyWhitelisted onlyFreeMint {
+        _freeMint(account, mintAmount);
     }
 
     /**
@@ -141,11 +186,20 @@ contract MinterSample is OwnableUpgradeable {
      * @param depositAmounts Array of token amounts to mint for each corresponding address
      * @notice The caller must send exactly the OAS amount that matches the sum of all amounts
      * @notice Contract must have OPERATOR_ROLE to execute this function
+     * @notice Restricted to non-free mint
      */
     function bulkMint(
         address[] calldata accounts,
         uint256[] calldata depositAmounts
-    ) public payable virtual hasOperatorRole onlyWhitelisted withinMintCap {
+    )
+        public
+        payable
+        virtual
+        hasOperatorRole
+        onlyWhitelisted
+        nonFreeMint
+        withinMintCap
+    {
         require(
             accounts.length == depositAmounts.length,
             "Arrays length mismatch"
@@ -157,12 +211,34 @@ contract MinterSample is OwnableUpgradeable {
             require(depositAmounts[i] > 0, "Empty amount");
 
             sum += depositAmounts[i];
-            poas.mint(accounts[i], _mintAmount(depositAmounts[i]));
+            poas.mint(accounts[i], calculateMintAmount(depositAmounts[i]));
         }
 
         require(sum == msg.value, "Sum of amounts mismatch value");
 
         _deposit();
+    }
+
+    function bulkFreeMint(
+        address[] calldata accounts,
+        uint256[] calldata mintAmounts
+    ) public virtual hasOperatorRole onlyWhitelisted onlyFreeMint {
+        require(
+            accounts.length == mintAmounts.length,
+            "Arrays length mismatch"
+        );
+
+        uint256 sum = 0;
+        for (uint256 i = 0; i < accounts.length; ++i) {
+            require(accounts[i] != address(0), "Empty address");
+            require(mintAmounts[i] > 0, "Empty amount");
+
+            sum += mintAmounts[i];
+            poas.mint(accounts[i], mintAmounts[i]);
+        }
+
+        _capCheck(sum); // check if the total mint amount is within the mint cap
+        _updateCap(sum);
     }
 
     /**
@@ -225,7 +301,6 @@ contract MinterSample is OwnableUpgradeable {
      * @param mintRate_ The new mint rate value expressed as a percentage where 100 = 100%
      */
     function updateMintRate(uint16 mintRate_) public onlyOwner {
-        require(mintRate_ > 0, "Rate must be greater than 0");
         require(mintRate_ < 10000, "Rate must be less than 10000");
 
         mintRate = mintRate_;
@@ -242,12 +317,68 @@ contract MinterSample is OwnableUpgradeable {
     }
 
     /**
-     * @dev Internal function to calculate the mint amount based on the deposit amount
-     * @param amount The amount of OAS to deposit (in wei)
+     * @dev Calculate the mint amount based on the deposit amount
+     * @param depositAmount The amount of OAS to deposit (in wei)
      * @return The calculated mint amount based on the mint rate
      */
-    function _mintAmount(uint256 amount) public view virtual returns (uint256) {
-        return (amount * mintRate) / 100;
+    function calculateMintAmount(
+        uint256 depositAmount
+    ) public view virtual returns (uint256) {
+        if (isFreeMint()) {
+            revert("No deposit needed for free mint");
+        }
+        return (depositAmount * mintRate) / 100;
+    }
+
+    /**
+     * @dev Calculate the deposit amount based on the mint amount
+     * @param mintAmount The amount of tokens to mint
+     * @return The calculated deposit amount based on the mint rate
+     */
+    function calculateDepositAmount(
+        uint256 mintAmount
+    ) public view virtual returns (uint256) {
+        return isFreeMint() ? 0 : (mintAmount * 100) / mintRate;
+    }
+
+    /**
+     * @dev Internal function to check if the mint is free
+     * @return True if the mint is free, false otherwise
+     */
+    function isFreeMint() public view virtual returns (bool) {
+        return mintRate == 0;
+    }
+
+    /**
+     * @dev Internal function to check if the mint amount is within the mint cap
+     * @param mintAmount The amount of tokens to mint
+     * @notice Checks if the total mint amount is within the mint cap
+     * @notice Checks if the individual mint amount is within the whitelist allowance
+     */
+    function _capCheck(uint256 mintAmount) internal view virtual {
+        require(
+            mintedAmount + mintAmount <= mintCap,
+            "Total mint cap exceeded"
+        );
+        if (!disableWhitelistCheck) {
+            require(
+                whitelistWithAllowanceMap[msg.sender] >= mintAmount,
+                "Mint cap exceeded"
+            );
+        }
+    }
+
+    /**
+     * @dev Internal function to update the minted amount and the whitelist allowance
+     * @param mintAmount The amount of tokens to mint
+     * @notice Updates the minted amount
+     * @notice Updates the whitelist allowance
+     */
+    function _updateCap(uint256 mintAmount) internal virtual {
+        mintedAmount += mintAmount;
+        if (!disableWhitelistCheck) {
+            whitelistWithAllowanceMap[msg.sender] -= mintAmount;
+        }
     }
 
     /**
@@ -261,8 +392,24 @@ contract MinterSample is OwnableUpgradeable {
         require(account != address(0), "Empty address");
         require(depositAmount == msg.value, "Amount mismatch");
 
-        poas.mint(account, _mintAmount(depositAmount));
+        poas.mint(account, calculateMintAmount(depositAmount));
         _deposit();
+    }
+
+    /**
+     * @dev Internal function to free mint tokens for a single account
+     * @param account The address that will receive the minted tokens
+     * @param mintAmount The amount of tokens to mint
+     * @notice Checks if the mint amount is within the mint cap
+     * @notice Updates the minted amount and the whitelist allowance
+     */
+    function _freeMint(address account, uint256 mintAmount) internal virtual {
+        require(account != address(0), "Empty address");
+        require(mintAmount > 0, "Empty amount");
+        _capCheck(mintAmount);
+
+        poas.mint(account, mintAmount);
+        _updateCap(mintAmount);
     }
 
     /**
